6f1573240034cd0f13db1b1b2fc9e7571c526bee
小陳 <tim.minecraft2017@gmail.com>
2024-06-20 10:32:28 +0800

    master

---
+++ app/src/main/java/com/example/pojecttest/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.pojecttest
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Date
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        var textview : TextView = findViewById(R.id.time_date)
+
+        textview.text = Date().hours.toString()
+    }
+}
