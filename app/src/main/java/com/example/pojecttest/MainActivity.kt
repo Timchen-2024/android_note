223ea7992ee0ec065d0592bebd3b5893e03696f3
Timchen-2024 <tim.minecraft2017@gmail.com>
2024-06-20 21:46:19 +0800

    map_edit

--- app/src/main/java/com/example/pojecttest/MainActivity.kt
+++ app/src/main/java/com/example/pojecttest/MainActivity.kt
@@ -9,18 +9,24 @@ import org.osmdroid.tileprovider.tilesource.XYTileSource
 import org.osmdroid.views.MapView
 import java.util.Date
 import org.osmdroid.config.Configuration.*
+import org.osmdroid.tileprovider.modules.OfflineTileProvider
+import org.osmdroid.tileprovider.util.SimpleRegisterReceiver
+import org.osmdroid.util.GeoPoint
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
         setContentView(R.layout.activity_main)
+        val filepath = "/data/data/com.examaple.projecttest/files/osmdroid/map.sqlite"
+
         var textview : TextView = findViewById(R.id.time_date)
 
         textview.text = Date().hours.toString()
 
         var mapID : MapView = findViewById(R.id.mapID)
-        getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
 
         mapID.setUseDataConnection(false)
         mapID.setTileSource(
@@ -33,5 +39,16 @@ class MainActivity : AppCompatActivity() {
                 arrayOf("")
             )
         )
+        val file = File(filepath)
+        val tileProvider_ = OfflineTileProvider(
+            SimpleRegisterReceiver(this@MainActivity),
+            arrayOf(file)
+        )
+        mapID.tileProvider = tileProvider_
+
+        val mapctrl = mapID.controller
+        mapctrl.setZoom(9.5)
+        val startpoint = GeoPoint(48.8583,2.2944)
+        mapctrl.setCenter(startpoint)
     }
 }
